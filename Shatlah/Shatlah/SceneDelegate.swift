b06b98a83793a37719a8f9a0eee7fae3a0399054
wejdan <Wejdan.ibrahim.ruh.s@tuwaiq.edu.sa>
2021-12-18 00:02:20 +0300

    class profileuser-fetchdata-animationclass(wejdan

--- Shatlah/Shatlah/SceneDelegate.swift
+++ Shatlah/Shatlah/SceneDelegate.swift
@@ -13,23 +13,28 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         FirebaseApp.configure()
-        var rootViewController : UIViewController?
-        if Auth.auth().currentUser?.uid == nil{
-             rootViewController = LoginViewController()
-        }else{
-            rootViewController = DashboardTabBarController()
-        }
-        let navigationController = UINavigationController(rootViewController: rootViewController!)
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = LoginViewController()
-        window?.makeKeyAndVisible()
-        
-        
-        
+//        var rootViewController : UIViewController?
+//        if Auth.auth().currentUser?.uid == nil{
+//             rootViewController = LoginViewController()
+//        }else{
+//            rootViewController = DashboardTabBarController()
+//        }
+//        let navigationController = UINavigationController(rootViewController: rootViewController!)
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+//        window?.windowScene = windowScene
+//        window?.rootViewController = LoginViewController()
+//        window?.makeKeyAndVisible()
+/////////////
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    let window = UIWindow(windowScene: windowScene)
+    window.rootViewController = UserProfileController()
+    window.makeKeyAndVisible()
+    self.window = window
+}
+      ///////////////
       //  guard let _ = (scene as? UIWindowScene) else { return }
-    }
+
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
@@ -58,7 +63,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
+
+
 
