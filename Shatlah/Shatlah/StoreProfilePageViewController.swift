6eb66b05cdca6b21e2df4373e30f6fae2caf02e4
mohamed_cz <mohammed.ali.ruh.s@tuwaiq.edu.sa>
2021-12-18 20:39:22 +0300

    table view added

--- Shatlah/Shatlah/StoreProfilePageViewController.swift
+++ Shatlah/Shatlah/StoreProfilePageViewController.swift
@@ -1,58 +1,259 @@
 import UIKit
-import Firebase
+import FirebaseStorage
+import FirebaseAuth
+import FirebaseFirestore
 
 class StoreProfilePageViewController: UIViewController {
+    let db = Firestore.firestore()
+    let imagePicker = UIImagePickerController()
+    let storage = Storage.storage()
 
-  var stores : Store?
-  let db = Firestore.firestore()
-  var storeImageProfile = UIImageView(frame: CGRect(x: 50, y: 30, width: 150, height: 150))
-  var storeNameLable = UILabel(frame: CGRect(x: 50, y: 40, width: 100, height: 50))
-  var StoreDescriptionLabe = UILabel(frame: CGRect(x: 50, y: 150, width: 200, height: 200))
-  var addPlanetTypeButton = UIButton(frame: CGRect(x: 50, y: 300, width: 60, height: 30))
-  var storeAccount = UILabel(frame: CGRect(x: 50, y: 45, width: 100, height: 50))
-  override func viewDidLoad() {
-    storeImageProfile.image = UIImage(systemName: "person")
-//      storeNameLable.text =
-    storeNameLable.text = stores?.storetName
-    storeAccount.text = stores?.storeEmail
-    super.viewDidLoad()
-    view.backgroundColor = .white
-    view.addSubview(storeImageProfile)
-    view.addSubview(storeNameLable)
-    view.addSubview(StoreDescriptionLabe)
-    view.addSubview(addPlanetTypeButton)
-    view.addSubview(storeAccount)
-    // Do any additional setup after loading the view.
-    DispatchQueue.global(qos: .userInitiated).async {
-      DispatchQueue.main.async {
-        self.db.collection("Stores").getDocuments { querySnapshot, error in
-          if let error = error{
-            print("wrong read!!")
-            print(error.localizedDescription)
-            }else {
-              guard querySnapshot != nil else{return}
-              for document in querySnapshot!.documents {
-//                let dsplaytname = document.get("displayName") as! String
-//                var email = as? String
-//                var storeID = as? String
-//                var isstore = as? String
-//
-                var s1 : Store? = nil
-                s1?.storeEmail = document.get("email") as? String ?? "dddd"
-                s1?.storeID = document.get("userID") as? String ?? "dddd"
-                s1?.storetName = document.get("isStore") as? String ?? "dddd"
-                self.stores = s1
-    //            self.stores.append(Store(storetName: dsplaytname, storeEmail: email!,storeID: storeID!, isStore: isstore))
-              }
-    //
-    //  //        print(data["username"] as? String ?? "not valid username!")
-    //  //        print(data["text"] as? String ?? "not valid text!" )
-    //
-    //        }
-    //        let data : [String: Any] = ["displayName":self.firstName.text!,"email":self.email.text!,"userID":Auth.auth().currentUser!.uid, "isStore": "state" ]
-        }
-      }
-      }
+    let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(#colorLiteral(red: 0.2687272429, green: 0.3382961154, blue: 0.2888988853, alpha: 1))
+        view.layer.cornerRadius = 13
+        view.layer.cornerCurve = .continuous
+        view.clipsToBounds = true
+        return view
+    }()
+
+    let profileImage: UIImageView = {
+        let pI = UIImageView()
+        pI.contentMode = .scaleAspectFit
+        pI.clipsToBounds = true
+        pI.layer.cornerRadius = 80
+        pI.layer.borderColor = UIColor.white.cgColor
+        pI.layer.borderWidth = 1
+        pI.image = UIImage(systemName: "house.fill")
+        return pI
+    }()
+
+    let userNameLabel: UILabel = {
+        let name = UILabel()
+        name.font = UIFont.systemFont(ofSize: 29, weight: .bold)
+        name.textColor = .white
+        name.textAlignment = .center
+
+        return name
+    }()
+
+
+    let userEmailLabel: UILabel = {
+        let email = UILabel()
+        email.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        email.textAlignment = .center
+        email.textColor = .white
+        return email
+    }()
+
+    let signOutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign out", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray6
+        title = "Store Profile"
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+        readImageFromFirestore()
+        setUpLabels()
     }
-  }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchCurrentUsers()
+    }
+    func setUpLabels() {
+
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(containerView)
+
+        containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive                                = true
+        containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive                                = true
+        containerView.widthAnchor.constraint(equalToConstant: 325).isActive                                         = true
+        containerView.heightAnchor.constraint(equalToConstant: 375).isActive                                        = true
+
+        profileImage.tintColor  = .white
+        profileImage.isUserInteractionEnabled = true
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+        profileImage.addGestureRecognizer(tapRecognizer)
+
+        containerView.addSubview(profileImage)
+
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20).isActive = true
+        profileImage.widthAnchor.constraint(equalToConstant: 160).isActive = true
+        profileImage.heightAnchor.constraint(equalToConstant: 160).isActive = true
+        profileImage.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(userNameLabel)
+        userNameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 20).isActive = true
+        userNameLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+
+
+
+        containerView.addSubview(signOutButton)
+
+
+        userEmailLabel.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(userEmailLabel)
+        userEmailLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+        userEmailLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 3).isActive = true
+
+        containerView.addSubview(signOutButton)
+
+        signOutButton.topAnchor.constraint(equalTo: userEmailLabel.bottomAnchor, constant: 20).isActive = true
+        signOutButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20).isActive = true
+        signOutButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20).isActive = true
+        signOutButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        signOutButton.addTarget(self, action: #selector(signOutButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func signOutButtonTapped() {
+        guard let currentUser = Auth.auth().currentUser else {return}
+        do{
+            try Auth.auth().signOut()
+        }catch{print("Error")}
+    }
+
+    func setupImagePicker() {
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true)
+    }
+    
+    @objc func imageTapped() {
+        print("Image tapped")
+        setupImagePicker()
+    }
+
+    func saveImageToFirestore(url: String, userId: String) {
+
+        db.collection("Stores").document(userId).setData([
+            "userImageURL": url,
+        ], merge: true) { err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+            }
+        }
+    }
+
+    private func readImageFromFirestore(){
+        guard let currentUser = Auth.auth().currentUser else {return}
+
+        db.collection("Stores").whereField("userID", isEqualTo: currentUser.uid)
+            .addSnapshotListener { (querySnapshot, error) in
+                if let e = error {
+                    print("There was an issue retrieving data from Firestore. \(e)")
+                } else {
+
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+
+                            if let imageURL = data["userImageURL"] as? String
+                            {
+
+                                let httpsReference = self.storage.reference(forURL: imageURL)
+
+
+                                httpsReference.getData(maxSize: 1 * 1024 * 1024) { data, error in
+                                    if let error = error {
+                                        print("ERROR GETTING DATA \(error.localizedDescription)")
+                                    } else {
+                                        DispatchQueue.main.async {
+                                            self.profileImage.image = UIImage(data: data!)
+                                        }
+
+                                    }
+                                }
+
+                            } else {
+
+                                print("error converting data")
+                                DispatchQueue.main.async {
+                                    self.profileImage.image = UIImage(systemName: "house.fill")
+                                }
+
+                            }
+
+
+                        }
+                    }
+                }
+            }
+    }
+
+    private func fetchCurrentUsers() {
+        guard let currentUser = FirebaseAuth.Auth.auth().currentUser else {return}
+        db.collection("Stores").whereField("userID", isEqualTo: currentUser.uid)
+            .addSnapshotListener { (querySnapshot, error) in
+                if let e = error {
+                    print("There was an issue retrieving data from Firestore. \(e)")
+                } else {
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        print(snapshotDocuments)
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+                            if let userName = data["storeName"] as? String,
+                               let userEmail = data["email"] as? String
+                            {
+                                DispatchQueue.main.async {
+                                    self.userNameLabel.text = userName
+                                    self.userEmailLabel.text = userEmail
+                                }
+
+
+                            }
+                        }
+                    }
+                }
+
+            }
+    }
+
+}
+
+extension StoreProfilePageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+
+        guard let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {return}
+        guard let d: Data = userPickedImage.jpegData(compressionQuality: 0.5) else { return }
+        guard let currentUser = Auth.auth().currentUser else {return}
+
+
+
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/png"
+
+        let ref = storage.reference().child("StoreProfileImages/\(currentUser.email!)/\(currentUser.uid).jpg")
+
+        ref.putData(d, metadata: metadata) { (metadata, error) in
+            if error == nil {
+                ref.downloadURL(completion: { (url, error) in
+                    self.saveImageToFirestore(url: "\(url!)", userId: currentUser.uid)
+
+                })
+            }else{
+                print("error \(String(describing: error))")
+            }
+        }
+
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+
+
 }
--- Shatlah/Shatlah/StoreViewController.swift
+++ Shatlah/Shatlah/StoreViewController.swift
@@ -1,90 +1,146 @@
-// StoreViewController.swift
-// ShatlahApp
-//
-// Created by Taraf Bin suhaim on 11/05/1443 AH.
-//
 import UIKit
 import Firebase
 
-class StoreViewController: UIViewController, UISearchBarDelegate {
+class StoreViewController: UIViewController {
     
-    private let db = Firestore.firestore()
-    private var stores : [Store] = []
-   
+     let db = Firestore.firestore()
+     let searchBar = UISearchController()
+     var stores : [Store] = []
+     var filteredResults: [Store] = []
+     let tableView  = UITableView(frame: .zero, style: .insetGrouped)
+     var imagesArr = [UIImage]()
+     let storage = Storage.storage()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        title = "Store"
         uiSettengs()
         getData()
     }
-    
-    lazy var searchBar:UISearchBar = {
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.searchBarStyle = UISearchBar.Style.default
-        $0.placeholder = "Search..."
-        $0.sizeToFit()
-        $0.isTranslucent = false
-        $0.delegate = self
-        $0.backgroundImage = UIImage()
-        return $0
-    }(UISearchBar())
-    lazy var tableView : UITableView = {
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.register(StoreCell.self, forCellReuseIdentifier: StoreCell.id)
-        $0.rowHeight = UITableView.automaticDimension
-        $0.estimatedRowHeight = 400
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.delegate = self
-        $0.dataSource = self
-        $0.layer.cornerRadius = 10
-        return $0
-    }(UITableView())
-    
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+
     func uiSettengs(){
-        [searchBar,tableView].forEach{view.addSubview($0)}
-        NSLayoutConstraint.activate([
-            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20),
-            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -20),
-            
-            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 20),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
+    
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(StoreCell.self, forCellReuseIdentifier: StoreCell.id)
+        tableView.rowHeight                                                     = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 400
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        
+        searchBar.loadViewIfNeeded()
+        searchBar.searchResultsUpdater = self
+        searchBar.obscuresBackgroundDuringPresentation = false
+        searchBar.searchBar.returnKeyType = .done
+        searchBar.searchBar.sizeToFit()
+        searchBar.searchBar.placeholder = "Search..."
+        searchBar.hidesNavigationBarDuringPresentation = false
+        definesPresentationContext = true
+        
+        navigationItem.searchController = searchBar
+        navigationItem.hidesSearchBarWhenScrolling = true
+        searchBar.searchBar.delegate = self
     }
     
     func getData(){
         db.collection("Stores")
             .addSnapshotListener { (querySnapshot, error) in
-                self.stores = []
+               
                 if let e = error {
                     print("There was an issue retrieving data from Firestore. \(e)")
                 } else {
+                    self.stores = []
                     for document in querySnapshot!.documents{
                         let data = document.data()
-                        self.stores.append(Store(storetName: data["storeName"] as? String ?? "NA", storeEmail: data["email"] as? String ?? "NA"))
-                        print(data["storeName"] as? String ?? "NA")
+                        self.stores.append(Store(storetName: data["storeName"] as? String ?? "NA", storeEmail: data["email"] as? String ?? "NA", userImageURL: data["userImageURL"] as? String ?? "NA"))
+                        
                     }
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
                     }
+                    
                 }
             }
     }
+
+    func readImageFromFirestore(with url: String, completion: @escaping (UIImage) -> ()){
+        if  url != "NA"
+        {
+            let httpsReference = self.storage.reference(forURL: url)
+            httpsReference.getData(maxSize: 1 * 1024 * 1024) { data, error in
+                if let error = error {
+                    print("ERROR GETTING DATA \(error.localizedDescription)")
+                } else {
+                    completion(UIImage(data: data!) ?? UIImage())
+                }
+            }
+            
+        }
+    }
+
 }
 
 extension StoreViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return stores.count
+        if searchBar.isActive && !searchBar.searchBar.text!.isEmpty {
+            return filteredResults.count
+        }else{
+            return stores.count
+        }
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: StoreCell.id, for: indexPath) as! StoreCell
-        cell.stroeName.text = stores[indexPath.row].storetName
-        cell.storeEmail.text = stores[indexPath.row].storeEmail
-        return cell
+        
+        if searchBar.isActive && !searchBar.searchBar.text!.isEmpty {
+            cell.stroeName.text = filteredResults[indexPath.row].storetName
+            cell.storeEmail.text = filteredResults[indexPath.row].storeEmail
+            
+            return cell
+        }else{
+          
+            cell.stroeName.text = stores[indexPath.row].storetName
+            cell.storeEmail.text = stores[indexPath.row].storeEmail
+            cell.storeImage.image = UIImage(systemName: "house.fill")?.withTintColor(.black).withRenderingMode(.alwaysOriginal)
+            
+            readImageFromFirestore(with: stores[indexPath.row].userImageURL ?? "NA") { image in
+                DispatchQueue.main.async {
+                    cell.storeImage.image = image
+                }
+            }
+            return cell
+        }
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
+    }
+}
+
+extension StoreViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        if !searchController.isActive {
+            return
+        }
+        let searchBar = searchBar.searchBar
+        if let userEnteredSearchText = searchBar.text {
+            findResultsBasedOnSearch(with: userEnteredSearchText)
+        }
+    }
+    
+    func findResultsBasedOnSearch(with text: String)  {
+        filteredResults.removeAll()
+        if !text.isEmpty {
+            filteredResults = stores.filter{ item in
+                item.storetName.lowercased().contains(text.lowercased())
+            }
+            tableView.reloadData()
+        }else{
+            tableView.reloadData()
+        }
     }
 }
